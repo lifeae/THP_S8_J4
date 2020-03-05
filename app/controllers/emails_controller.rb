f135bc1afa07c3b2bf77561a91f9091caadb7ba4
lifeae <romain.leporatti@laposte.net>
2020-03-05 14:15:41 +0100

    seeds.rb :heavy_check_mark:

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,23 @@
+class EmailsController < ApplicationController
+before_action :authenticate_user!
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
